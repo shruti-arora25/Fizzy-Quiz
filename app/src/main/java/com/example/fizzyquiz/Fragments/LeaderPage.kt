08d8073ab16bf0032f7173222573f2099362edab
Shruti <shrutiarora2507@gmail.com>
2024-04-05 19:19:32 +0530

    Comit

---
+++ app/src/main/java/com/example/fizzyquiz/Fragments/LeaderPage.kt
@@ -0,0 +1,28 @@
+package com.example.fizzyquiz.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.fizzyquiz.R
+
+
+class LeaderPage : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_leader_page, container, false)
+    }
+
+
+}
