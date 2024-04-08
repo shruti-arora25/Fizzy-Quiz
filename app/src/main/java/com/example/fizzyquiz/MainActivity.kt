23fccd038e5a65c8fbd9a4d3342e248cbd2cbc31
Shruti <shrutiarora2507@gmail.com>
2024-04-08 17:26:08 +0530

    Comit

--- app/src/main/java/com/example/fizzyquiz/MainActivity.kt
+++ app/src/main/java/com/example/fizzyquiz/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.fizzyquiz
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.example.fizzyquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
-
+    private lateinit var navC: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +21,13 @@ class MainActivity : AppCompatActivity() {
         window.statusBarColor = ContextCompat.getColor(this@MainActivity, R.color.light_grey)
 
 
-
-
-
-
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navC=findNavController(R.id.fragmentContainerView)
+
+        return navC.navigateUp()||super.onSupportNavigateUp()
+    }
+
+
 }
